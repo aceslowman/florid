729c690be16780ccce703ea5dd7be79c1dd75766
Glitch (hello-webpage) <none>
2021-02-24 04:12:51 +0000

    🌒🚋 Checkpoint ./script.js:24872531/991 ./style.css:24872531/398 ./index.html:24872531/1602

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// https://stackoverflow.com/questions/4954252/css-textarea-that-expands-as-you-type-text
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function updateSize(e) {
+  let text = e.target.value + String.fromCharCode(event.keyCode);
+  e.target.rows = text.split(/\r\n|\r|\n/).length;
+}
+
+function keyDownUpdateSize(e) {
+  if (event.keyCode != 8 && event.keyCode != 46)
+    updateSize(e);
+}
+
+function keyUpUpdateSize(e) {
+  if (event.keyCode == 8 || event.keyCode == 46)
+    updateSize(e);
+}
+
+document.querySelector(".MAININPUT").addEventListener("keydown", keyDownUpdateSize);
+document.querySelector(".MAININPUT").addEventListener("keyup", keyUpUpdateSize)
