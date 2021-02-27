84b1cb51a115e67786b4668cf756c9f92e286309
Glitch (hello-webpage) <none>
2021-02-27 18:19:34 +0000

    🌛👜 Checkpoint ./.eslintignore:24872531/9 ./script.js:24872531/57652 ./toggleSettings.js:24872531/912 ./index.html:24872531/65

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// CENTERS MAININPUT TEXT
-// https://stackoverflow.com/questions/4954252/css-textarea-that-expands-as-you-type-text
-function updateSize(e) {
-  let text = e.target.value + String.fromCharCode(event.keyCode);
-  e.target.rows = text.split(/\r\n|\r|\n/).length;
-}
-
-function keyDownUpdateSize(e) {
-  if (event.keyCode != 8 && event.keyCode != 46) updateSize(e);
-}
-
-function keyUpUpdateSize(e) {
-  if (event.keyCode == 8 || event.keyCode == 46) updateSize(e);
-}
-
-document
-  .querySelector(".MAININPUT")
-  .addEventListener("keydown", keyDownUpdateSize);
-document.querySelector(".MAININPUT").addEventListener("keyup", keyUpUpdateSize);
-
-// SETTINGS TOGGLE
-function toggleSettings() {
-  let settingsPanel = document.querySelector(".SETTINGS");
-  settingsPanel.style.width = settingsPanel.style.width === "0%" ? "28%" : "0%";
-}
-
-document
-  .querySelector(".toggleSettings")
-  .addEventListener("click", toggleSettings);
-
 // DISPLAY TICKS AS THEY CHANGE
 let state = {
   tick: 0,
@@ -43,21 +13,17 @@ document.querySelector(".pauseInput").value = state.pauseAfterLine;
 // MAIN TICK LOOP
 setInterval(() => {
   state.tick += 1;
-  const indicator = document.querySelector(".tickIndicator");
-  indicator.textContent = `tick: ${state.tick}`;
+  document.querySelector(".tickIndicator").textContent = `tick: ${state.tick}`;
 }, state.tempo);
 
 // MAIN TEXT INPUT PARSING
 function handleMainTextChange(e) {
-  state.txtArray = e.target.value.split(/\r?\n/).map(e => e.split(" "));
-
   // parse lines and units
-  // console.log("TXT", state.txtArray);
+  state.txtArray = e.target.value.split(/\r?\n/).map(e => e.split(" "));
 }
 
 document.querySelector(".MAININPUT").addEventListener("keydown", e => {
   // disallow anything that isn't a dot, slash, new line, space, or backspace
-  console.log(e.keyCode);
   if (
     e.keyCode !== 190 &&
     e.keyCode !== 191 &&
@@ -70,7 +36,6 @@ document.querySelector(".MAININPUT").addEventListener("keydown", e => {
 });
 
 let part;
-
 let synth = new Tone.Synth().toDestination();
 
 document.querySelector(".MAININPUT").addEventListener("keyup", async e => {
@@ -83,11 +48,11 @@ document.querySelector(".MAININPUT").addEventListener("keyup", async e => {
     restartSynth();
   } else {
     part.clear();
-    
+
     getPartFromText().forEach(e => {
       part.add(e);
     });
-    
+
     part.loop = true;
   }
 
@@ -101,7 +66,12 @@ function restartSynth() {
   // use an array of objects as long as the object has a "time" attribute
   part = new Tone.Part((time, value) => {
     // the value is an object which contains both the note and the velocity
-    synth.triggerAttackRelease(value.note, value.duration, time, value.velocity);
+    synth.triggerAttackRelease(
+      value.note,
+      value.duration,
+      time,
+      value.velocity
+    );
   }, getPartFromText()).start(0);
 
   part.loop = true;
---
+++ toggleSettings.js
@@ -0,0 +1,30 @@
+// CENTERS MAININPUT TEXT
+// https://stackoverflow.com/questions/4954252/css-textarea-that-expands-as-you-type-text
+function updateSize(e) {
+  let text = e.target.value + String.fromCharCode(event.keyCode);
+  e.target.rows = text.split(/\r\n|\r|\n/).length;
+}
+
+function keyDownUpdateSize(e) {
+  if (event.keyCode != 8 && event.keyCode != 46) updateSize(e);
+}
+
+function keyUpUpdateSize(e) {
+  if (event.keyCode == 8 || event.keyCode == 46) updateSize(e);
+}
+
+document
+  .querySelector(".MAININPUT")
+  .addEventListener("keydown", keyDownUpdateSize);
+document.querySelector(".MAININPUT").addEventListener("keyup", keyUpUpdateSize);
+
+// SETTINGS TOGGLE
+function toggleSettings() {
+  let settingsPanel = document.querySelector(".SETTINGS");
+  settingsPanel.style.width = settingsPanel.style.width === "0%" ? "28%" : "0%";
+}
+
+document
+  .querySelector(".toggleSettings")
+  .addEventListener("click", toggleSettings);
+
