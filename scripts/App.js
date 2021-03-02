38f69712d0980334d3c0a5b817a4e96dcd9d7f48
Glitch (hello-webpage) <none>
2021-03-02 18:27:20 +0000

    👰🏙 Checkpoint ./scripts/App.js:24872531/8918

--- scripts/App.js
+++ scripts/App.js
@@ -15,25 +15,26 @@ const App = () => {
   let [midiOutputs, setMidiOutputs] = React.useState([]);
 
   React.useEffect(() => {
-    const initializeMIDI = async (access) => {
-    
-      // Get lists of available MIDI controllers
-      const inputs = access.inputs.values();
-      const outputs = access.outputs.values();
-      
-      console.log("MIDI INPUTS", [...inputs]);
-      console.log("MIDI OUTPUTS", [...outputs]);
-      
-      setMidiInputs([...inputs]);
-      setMidiOutputs([...outputs]);
+    const initializeMIDI = async () => {
+      await navigator.requestMIDIAccess().then(function(access) {
+        // Get lists of available MIDI controllers
+        const inputs = access.inputs.values();
+        const outputs = access.outputs.values();
 
-      access.onstatechange = function(e) {
-        // Print information about the (dis)connected MIDI controller
-        console.log(e.port.name, e.port.manufacturer, e.port.state);
-      };
+        console.log("MIDI INPUTS", [...inputs]);
+        console.log("MIDI OUTPUTS", [...outputs]);
+
+        setMidiInputs([...inputs]);
+        setMidiOutputs([...outputs]);
+
+        access.onstatechange = function(e) {
+          // Print information about the (dis)connected MIDI controller
+          console.log(e.port.name, e.port.manufacturer, e.port.state);
+        };
+      });
+    };
     
-    }
-    
+    initializeMIDI();
   }, []);
 
   function handleLoopToggle(e) {
@@ -85,16 +86,16 @@ const App = () => {
 
     setMelody(newMelody);
   }
-  
+
   function handleMidiInputChange(input) {
-    console.log('input', input)
+    console.log("input", input);
   }
-  
+
   function handleMidiOutputChange(output) {
-    console.log('output', output)
-  }  
-  
-  console.log('check this', midiInputs)
+    console.log("output", output);
+  }
+
+  console.log("check this", midiInputs);
 
   return (
     <React.Fragment>
